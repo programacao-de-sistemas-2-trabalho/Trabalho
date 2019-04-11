f7c08e6222e5ce74c4688c4dce6d3cd10d181724
Antonio2032 <48036333+Antonio2032@users.noreply.github.com>
2019-04-11 11:11:10 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,39 @@
+public class Produto {
+    private long id;
+    private String descricao;
+    private String marca;
+    private double preco;
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+  
+}
