c8f729990ee5db464f2fc87d9652af92bb2a1c7c
Antonio2032 <48036333+Antonio2032@users.noreply.github.com>
2019-04-21 19:46:21 -0300

    Update DBTime.java

--- DBTime.java
+++ DBTime.java
@@ -10,16 +10,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.List;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author fernandokasemodel
- */
 public class DBtime implements CRUDTime{
 
     private Connection getConnection() {
